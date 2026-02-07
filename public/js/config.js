e1818e0cf1e97865d0325b658a0518f928c88911
Brahim <merkezattawhid@gmail.com>
2026-02-07 22:44:42 +0100

    Fix gradient title style

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,3 @@
 // Configuration globale (dev)
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwt2yJYxBItIk5DknGjpHyvNaFhCCgGZmzOwP5CaSDc-GLIvBXoWVPJey7gkc1T9KwEBw/exec"};
+  API_URL: "https://script.google.com/macros/s/AKfycbwAIP7Swv23B-iVnCYEH5eNbZgOXhsbpU3Yb-wMJnGpeEN4VulUh5kBB3PMbxI2Ga0sJg/exec"};
